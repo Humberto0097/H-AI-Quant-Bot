7c4ca8457641b77c0a46687dc362ac38fcebc249
Humberto0097 <106354124+Humberto0097@users.noreply.github.com>
2026-03-05 23:41:04 -0500

    Add files via upload

--- app_premium.py
+++ app_premium.py
@@ -327,7 +327,6 @@ st.markdown("""
     /* Ocultar elementos de Streamlit (Github, Menu, Footer) */
     #MainMenu {visibility: hidden;}
     footer {visibility: hidden;}
-    header {visibility: hidden;}
 </style>
 """, unsafe_allow_html=True)
 
